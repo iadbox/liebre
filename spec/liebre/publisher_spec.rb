a86688e25f3216827ea0838b94f00a32a6134a8f
Raimon Malatesta <graimon@gmail.com>
2017-01-12 11:52:20 +0100

    fix specs

--- spec/liebre/publisher_spec.rb
+++ spec/liebre/publisher_spec.rb
@@ -30,7 +30,6 @@ RSpec.describe Liebre::Publisher do
     allow_any_instance_of(Liebre::Config).to receive(:rpc_request_timeout).and_return 10
     
     allow(Liebre::ConnectionManager).to receive(:instance).and_return connection_manager
-    expect(connection_manager).to receive(:channel_for).with(:default).and_return channel
     expect(connection_manager).to receive(:ensure_started)
     
     expect(Liebre::Common::Utils).to receive(:create_exchange).
@@ -41,6 +40,10 @@ RSpec.describe Liebre::Publisher do
   subject { described_class.new 'test_publisher' }
   
   describe "#enqueue" do
+    before do
+      expect(connection_manager).to receive(:channel_for).with(:default).and_return channel
+    end
+    
     it do
       message = "abc"
       expect(exchange).to receive(:publish).with message, {}
@@ -56,11 +59,15 @@ RSpec.describe Liebre::Publisher do
     let(:reply_queue)      { double "reply_queue", :name => reply_queue_name }
     let(:delivery_info)    { double "delivery_info", :consumer_tag => "tag"}
     let(:consumer)         { double 'consumer' }
+    let(:connection)       { double 'connection' }
     let(:consumers)        { {"tag" => consumer} }
     
     before do
+      expect(connection_manager).to receive(:get).with(:default).and_return connection
+      expect(connection).to receive(:create_channel).and_return channel
       expect(channel).to receive(:queue).with(reply_queue_name, :exclusive => true, :auto_delete => true).
         and_return reply_queue
+      expect(channel).to receive(:close)
     end
     
     it do
