0a0737000fd4d3c7f984823c140dbfd853af38b5
Raimon Malatesta <graimon@gmail.com>
2017-01-12 10:09:15 +0100

    add mutex for thread safety

--- lib/liebre/connection_manager.rb
+++ lib/liebre/connection_manager.rb
@@ -11,16 +11,19 @@ module Liebre
       @path = Liebre::Config.connection_path
       @connections = {}
       @channels = {}
+      @lock = Mutex.new
     end
 
     def start
-      initialize_connections
-      connections.each do |connection_name, bunny|
-        begin
-          bunny.start
-        rescue => e
-          $logger.error("#{self.class.name}: Can't connect to #{connection_name} instance")
-          $logger.error(e.message + "\n" + e.backtrace.join("\n"))
+      @lock.synchronize do
+        initialize_connections
+        connections.each do |connection_name, bunny|
+          begin
+            bunny.start
+          rescue => e
+            $logger.error("#{self.class.name}: Can't connect to #{connection_name} instance")
+            $logger.error(e.message + "\n" + e.backtrace.join("\n"))
+          end
         end
       end
     end
@@ -42,18 +45,22 @@ module Liebre
     end
     
     def channel_for connection_name, consumer_pool_size = 1
-      channels[connection_name] ||= begin
-        get(connection_name).create_channel nil, consumer_pool_size
+      @lock.synchronize do
+        channels[connection_name] ||= begin
+          get(connection_name).create_channel nil, consumer_pool_size
+        end
       end
     end
 
     def stop
-      connections.each do |_, bunny|
-        if bunny and bunny.open?
-          bunny.close
+      @lock.synchronize do
+        connections.each do |_, bunny|
+          if bunny and bunny.open?
+            bunny.close
+          end
         end
+        connections.clear
       end
-      connections.clear
     end
 
     private
