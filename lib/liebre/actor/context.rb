df32def877665d9de886dd43a77f3f72949869db
jcabotc <jcabot@gmail.com>
2017-03-10 09:32:35 +0100

    Revert "context instantiates resources_classes with a declare object instead of a channel"
    This reverts commit a5be6770928f78024e4e6a8f5e1f6195c85d97be.

--- lib/liebre/actor/context.rb
+++ lib/liebre/actor/context.rb
@@ -1,6 +1,5 @@
 require "liebre/actor/context/handler"
 require "liebre/actor/context/extensions"
-require "liebre/actor/context/declare"
 
 module Liebre
   module Actor
@@ -20,7 +19,7 @@ module Liebre
         @resources ||= begin
           spec = opts.fetch("resources", {})
 
-          resources_class.new(declare, spec)
+          resources_class.new(chan, spec)
         end
       end
 
@@ -36,10 +35,6 @@ module Liebre
       end
 
     private
-
-      def declare
-        @declare ||= Declare.new(chan)
-      end
 
       attr_reader :resources_class, :base_class
 
